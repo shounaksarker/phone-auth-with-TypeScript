16032890a5fcac8d7a296e8e9fdae272d85cd783
shounaksarker <shounaksarker@gmail.com>
2021-04-18 18:49:03 +0600

    netlify

--- src/Component/Authentication/firebaseConfig.ts
+++ src/Component/Authentication/firebaseConfig.ts
@@ -1,5 +1,16 @@
 const firebaseConfig = {
-    /* paste your firebase config here */
-  };
+  // apiKey: "AIzaSyBRGIcI5bqbwwPnkGWKvS-fw4hmSeT_IXM",
+  // authDomain: "phone-auth-snk.firebaseapp.com",
+  // projectId: "phone-auth-snk",
+  // storageBucket: "phone-auth-snk.appspot.com",
+  // messagingSenderId: "676588188004",
+  // appId: "1:676588188004:web:e6d703c252c013769b8a71"
+  apiKey: "AIzaSyCXIjcblL8hEqRBPmL6-Bx7dBtsMFQ8NW4",
+  authDomain: "powerodd-snk.firebaseapp.com",
+  projectId: "powerodd-snk",
+  storageBucket: "powerodd-snk.appspot.com",
+  messagingSenderId: "817299774504",
+  appId: "1:817299774504:web:8e474a9d441499076ca91d"
+};
 
 export default firebaseConfig;
